fe1b72a811db39d5d8d3fd2f4c544c3cedc34adc
Ana <ana.pinzari@icloud.com>
2022-02-19 23:29:58 +0200

    Created classes

---
+++ TheWedding/TheWedding/Models/Godparent.cs
@@ -0,0 +1,9 @@
+﻿namespace TheWedding.Models
+{
+    class Godparent : Guest
+    {
+        public Godparent(string firstName, string lastName, int monetaryGift) : base(firstName, lastName, monetaryGift)
+        {
+        }
+    }    
+}
---
+++ TheWedding/TheWedding/Models/Guest.cs
@@ -0,0 +1,12 @@
+﻿namespace TheWedding.Models
+{
+    class Guest : Person
+    {
+        public int MonetaryGift { get; set; }
+
+        public Guest(string firstName, string lastName, int monetaryGift) : base(firstName, lastName)
+        {
+            MonetaryGift = monetaryGift;
+        }
+    }
+}
